2b45c407add15e76b0c0d1f3ba2ba7a6f2ec3bde
Timo Salola <timo@salola.fi>
2017-01-14 12:16:11 +0200

    First release of the web page

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    setTimeout(() => {
+        $('#header').addClass('loaded');
+
+        setTimeout(() => {
+            $('#content').addClass('loaded');
+
+            let timeout = 300;
+            $('#content h2, #content p').each((i, elem) => {
+                showElemTimeout(elem, timeout * i);
+            });
+        }, 2000)
+    }, 500);
+});
+
+function showElemTimeout(elem, timeout) {
+    setTimeout(() => {
+        $(elem).addClass('loaded');
+    }, timeout)
+}
